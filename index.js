869ce2eb520ce42c61130b30500b9a21a2eca424
Arian Fornaris <arian@phasereditor2d.com>
2022-04-30 06:59:42 -0400

    Homepage: se muestran creadpores, estudios y productos de forma aleatoria.

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
+import { exit } from "process";
+import shuffleArray from "shuffle-array";
 import { SiteBuilder } from "staticojs";
 
 const builder = new SiteBuilder(".");
 
-builder.parse();
+const page = builder.parse();
+
+// random estudios for home page
+
+const estudios = page.$pages.find(p => p.$name === "estudios").$pages;
+
+shuffleArray(estudios);
+
+page.homepage_estudios = estudios.slice(0, 4);
+
+// random creators for the home page
+
+const creadores = page.$pages.find(p => p.$name === "creadores").$pages;
+
+shuffleArray(creadores);
+
+page.homepage_creadores = creadores.slice(0, 4);
+
+// random products
+
+const products = page.$pages.find(p => p.$name === "productos").$pages;
+
+shuffleArray(products);
+
+page.homepage_productos = products.slice(0, 4);
+
+// compile
 
 builder.compile();
